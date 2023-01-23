0ee099c63f76b4537caac2e995819a077abd8106
Piyush Verma <piyushverma476@gmail.com>
2023-01-23 15:07:40 +0530

    hardhat-deploy plugin installed installed and overloaded with more plugins

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,5 @@
+function deployFunc(){
+    console.log("Hello world");
+}
+
+module.exports.default = deployFunc;
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,37 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
+require("@nomiclabs/hardhat-etherscan");
+require("./tasks/block-number");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("hardhat-deploy");
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
-  solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        goerli: {
+            url: process.env.RPCURL,
+            accounts: [process.env.privateKey],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://127.0.0.1:8545/",
+            // accounts = [], hardhat placed an account for us here
+            chainId: 31337,
+
+        }
+    },
+    solidity: "0.8.17",
+    etherscan: {
+        apiKey: process.env.etherscanAPI,
+    },
+    gasReporter : {
+        enabled : true,
+        outputFile : "gas-report.txt",
+        noColors : true,
+        currency : "USD",
+        coinmarketcap : process.env.COIN_MKT_API,
+        token : "BITCOIN",
+    }
 };
